72e63adb2a144da1961b9f2f91c7ea1a9bdd6de2
Peter Kutschera <peter.kutschera@gmail.com>
2015-04-16 14:54:53 +0200

    Included ICMM patch

--- usr/local/wps/indicators/processes/crisma/ICMMtools.py
+++ usr/local/wps/indicators/processes/crisma/ICMMtools.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # Peter.Kutschera@ait.ac.at, 2014-03-13
-# Time-stamp: "2015-04-01 14:27:39 peter"
+# Time-stamp: "2015-04-16 14:19:18 peter"
 #
 # Tools to access ICMM
 
@@ -106,36 +106,60 @@ def getConstants (baseUrl=defaultBaseUrl, domain=defaultDomain):
             datadescriptorIdICCdata = r['id']
 
 
-def getId (clazz, baseUrl=defaultBaseUrl, domain=defaultDomain):
+def getIdOld (clazz, baseUrl=defaultBaseUrl, domain=defaultDomain):
     """Get the next available id for the given class
 
     clazz: class name within ICMM
     """
     params = {
-        'limit' :  999999999
+        'limit' :  999999999,
+        'level': 1,
+        'fields': None
         }
     headers = {'content-type': 'application/json'}
     response = requests.get("{0}/{1}.{2}".format (baseUrl, domain, clazz), params=params, headers=headers, verify=False) 
     # this was the request:
-    # print response.url
-
+    #print response.url
     if response.status_code != 200:
-        raise Exception ( "Error accessing ICMM at {0}: {1}".format (response.url, response.raise_for_status()))
-
+        raise Exception ( "Error accessing ICMM at {0}: {1}".format (response.url, response.status_code))
     # Depending on the requests-version json might be an field instead of on method
     # print response.json()
     jsonData = response.json() if callable (response.json) else response.json
-
     maxid = 0;
     collection = jsonData['$collection']
     for r in collection:
-        ref = r['$ref']
+        ref = r['$self']
         match = re.search ("/([0-9]+)$", ref)
         if (match):
             id = int (match.group(1))
             if (id > maxid):
                 maxid = id
     return maxid + 1
+
+
+def getId (clazz, baseUrl=defaultBaseUrl, domain=defaultDomain):
+    """Get the next available id for the given class
+
+    clazz: class name within ICMM
+    domain: NEED TO BE 'CRISMA'
+    """
+    if not "CRISMA" == domain:
+        raise Exception ("Only allowed domain is 'CRISMA'")
+    params = {
+        'domain': domain,
+        'class': clazz
+        }
+    headers = {'content-type': 'application/json'}
+    response = requests.get("{0}/nextId".format (baseUrl), params=params, headers=headers, verify=False) 
+    if response.status_code == 404:
+        # ICMM without Peter's Patch?
+        return getIdOld (clazz, baseUrl, domain)
+    if response.status_code != 200:
+        raise Exception ( "Error accessing ICMM at {0}: {1}".format (response.url, response.status_code))
+     # Depending on the requests-version json might be an field instead of on method
+    jsonData = response.json() if callable (response.json) else response.json
+    # print jsonData
+    return jsonData['nextId']
 
 
 def getNameDescription (wsid, baseUrl=defaultBaseUrl, domain=defaultDomain):
